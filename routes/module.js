3479b48b0d88da34cb093e6aa62daa21bef996cd
Kajal Chopade <chopadekajal01@gmail.com>
2025-09-19 00:25:47 +0530

    Added new API / get all modules

--- routes/module.js
+++ routes/module.js
@@ -5,5 +5,17 @@ const config = require("../config")
 const jwt = require('jsonwebtoken')
 
 const router = express.Router();
+// get all modules
+
+router.get("/modules", (request,response) => {
+    const statement = "SELECT module_id, module_name FROM module";
+    db.pool.query(statement, (error, result) => {
+
+      response.send(utils.createResult(error,result)) 
+
+    })
+})
+
+
 
 module.exports = router;
