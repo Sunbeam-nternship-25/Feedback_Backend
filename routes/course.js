b556b0dec85e042dc9f41688fecc718cecf8ef5a
Kajal Chopade <chopadekajal01@gmail.com>
2025-09-19 00:21:42 +0530

    Added new API / get all courses

--- routes/course.js
+++ routes/course.js
@@ -6,6 +6,14 @@ const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
+//get all courses
+router.get("/courses", (request,response) => {
+  const statement = "SELECT course_id, course_name FROM course";
+  db.pool.query(statement, (error, result) => {
+    response.send(utils.createResult(error,result));
+  })
+})
+
 
 
 module.exports = router;
