1f36abc1bcc96ab34c4c7dd2869b71ad03ad8744
Kajal Chopade <chopadekajal01@gmail.com>
2025-09-19 00:05:59 +0530

    Added new API / student feedback

--- routes/course.js
+++ routes/course.js
@@ -6,13 +6,6 @@ const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
-//get all courses
-router.get("/courses", (request,response) => {
-  const statement = "SELECT course_id, course_name FROM course";
-  db.pool.query(statement, (error, result) => {
-    response.send(utils.createResult(error,result));
-  })
-})
 
 
 module.exports = router;
--- routes/feedbackSchedule.js
+++ routes/feedbackSchedule.js
@@ -4,48 +4,6 @@ const utils = require("../utils");
 
 const router = express.Router();
 
-router.post("/createFeedback", async (request, response) => {
-
-  const {
-    teacher_id,
-    module_id,
-    module_type_id,
-    group_id,
-    course_id,
-    start_time,
-    end_time,
-    is_active
-  } = request.body;
-
-  
-  const statement = `insert into feedback_schedule( teacher_id,
-    module_id,
-    module_type_id,
-    group_id,
-    course_id,
-    start_time,
-    end_time,
-    is_active)values (?,?,?,?,?,?,?,?)`;
-
-  db.pool.execute(
-    statement,
-    [
-      teacher_id,
-      module_id,
-      module_type_id,
-      group_id,
-      course_id,
-      start_time,
-      end_time,
-      is_active
-    ],
-    (error, results) => {
-      response.send(utils.createResult(error, results));
-    }
-  );
-  
-});
-
 router.get("/activeFeedback", async (request, response) => {
   const statement = `
   SELECT 
@@ -95,59 +53,7 @@ router.get("/deActiveFeedback", async (request, response) => {
   });
 });
 
-router.put("/updateFeedback/:id", async (request, response) => {
-      const { id } = request.params
-  const {
-    teacher_id,
-    module_id,
-    module_type_id,
-    group_id,
-    course_id,
-    start_time,
-    end_time,
-  } = request.body;
 
-
-
-
-  const statement = `update feedback_schedule set
-  teacher_id = ?,
-    module_id =?,
-    module_type_id =?,
-    group_id =?,
-    course_id =?,
-    start_time =?,
-    end_time  =?
-    where feedback_schedule_id =?`;
-
-  db.pool.execute(
-    statement,
-    [
-      teacher_id,
-      module_id,
-      module_type_id,
-      group_id,
-      course_id,
-      start_time,
-      end_time,
-      id
-    ],
-    (error, results) => {
-      response.send(utils.createResult(error, results));
-    }
-  );
-});
-
-
-router.delete("/deleteFeedback/:id", async (request, response) => {
-  const { id } = request.params;
-
-  const statement = `DELETE FROM feedback_schedule WHERE feedback_schedule_id = ?`;
-
-  db.pool.execute(statement, [id], (error, results) => {
-    response.send(utils.createResult(error, results));
-  });
-});
 
 
 // To get data of FeedbackSchedule for Teacher 
